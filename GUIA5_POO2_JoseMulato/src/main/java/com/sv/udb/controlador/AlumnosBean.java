67f42833619f38aa70b981f8446ecd88b699c6fd
joseph <joseph@JoseMulato>
2016-09-11 17:24:18 -0600

    CRUD TERMINADO

--- GUIA5_POO2_JoseMulato/src/main/java/com/sv/udb/controlador/AlumnosBean.java
+++ GUIA5_POO2_JoseMulato/src/main/java/com/sv/udb/controlador/AlumnosBean.java
@@ -6,34 +6,34 @@
 package com.sv.udb.controlador;
 
 import com.sv.udb.modelo.Alumnos;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.ManagedBean;
 import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import javax.enterprise.context.RequestScoped;
+import javax.faces.view.ViewScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import org.primefaces.context.RequestContext;
 
 /**
  *
- * @author joseph
+ * @author REGISTRO
  */
 @Named(value = "alumnosBean")
-@RequestScoped
-public class AlumnosBean {
+@ViewScoped
+@ManagedBean
+
+public class AlumnosBean implements Serializable{
     private Alumnos objeAlum;
-    private boolean guar;
-        /**
-     * Creates a new instance of AlumnosBean
-     */
-   public AlumnosBean() {
-       
-    }
-    @PostConstruct // despues de iniciar la vista
-    public void init(){
-        this.objeAlum= new Alumnos();
-        this.guar= true;
-    }
+    private boolean guardar;
+    private List<Alumnos> alumnosList=null;
+
     public Alumnos getObjeAlum() {
         return objeAlum;
     }
@@ -41,9 +41,53 @@ public class AlumnosBean {
     public void setObjeAlum(Alumnos objeAlum) {
         this.objeAlum = objeAlum;
     }
+
+    public boolean isGuardar() {
+        return guardar;
+    }
     
-    public void guar(){
-        boolean resp = false;
+    public List<Alumnos> getAlumnosList(){
+        return alumnosList;
+    }
+    
+    public void setAlumnosList(List<Alumnos> alumnosList){
+        this.alumnosList=alumnosList;
+    }
+    
+    /**
+     * Creates a new instance of AlumnosBean
+     */
+    
+    public AlumnosBean() {
+    }
+    
+    @PostConstruct
+    public void init()
+    {
+        this.objeAlum = new Alumnos();
+        this.guardar = true;
+        this.alumnosList=this.ConsTodo();
+    }
+    
+    public List<Alumnos> ConsTodo() {
+        List<Alumnos> resp = new ArrayList<>();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("POOPU");
+        EntityManager em = emf.createEntityManager();
+        try 
+        {
+            TypedQuery<Alumnos> query = em.createNamedQuery("Alumnos.findAll", Alumnos.class);
+            resp = query.getResultList();
+        } 
+        catch (Exception ex) 
+        {
+
+        }
+        return resp;
+    }
+    
+    public void guar()
+    {
+        RequestContext ctx= RequestContext.getCurrentInstance();
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("POOPU");
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
@@ -52,7 +96,38 @@ public class AlumnosBean {
         {
             em.persist(this.objeAlum);
             tx.commit();
-            resp = true;
+            this.guardar = true;
+            this.objeAlum= new Alumnos();
+            this.alumnosList= this.ConsTodo();
+            ctx.execute("setMessage('MESS_SUCC','Alerta','Datos guardados')");
+        }
+        catch(Exception ex)
+        {
+            ctx.execute("setMessage('MESS_ERRO','Alerta','Error algo ha pasado')");
+            tx.rollback();
+            ex.printStackTrace();
+        }
+        finally
+        {
+            em.close();
+            emf.close();            
+        }
+    }
+    
+    public void modi()
+    {
+        RequestContext ctx = RequestContext.getCurrentInstance();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("POOPU");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try
+        {
+            em.merge(objeAlum);
+            tx.commit();
+            this.alumnosList = this.ConsTodo();
+            this.objeAlum = new Alumnos();
+            ctx.execute("setMessage('MESS_SUCC', 'Alerta', 'Datos modificado');");
         }
         catch(Exception ex)
         {
@@ -62,6 +137,46 @@ public class AlumnosBean {
         emf.close();
     }
     
-
+    public void elim(int codi)
+    {
+        RequestContext ctx = RequestContext.getCurrentInstance();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("POOPU");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try
+        {
+            Alumnos obj = em.find(Alumnos.class, codi);
+            em.remove(obj);
+            tx.commit();
+            this.alumnosList = this.ConsTodo();
+            this.objeAlum = new Alumnos();
+            ctx.execute("setMessage('MESS_SUCC', 'Alerta', 'Datos eliminados');");
+        }
+        catch(Exception ex)
+        {
+            tx.rollback();
+        }
+        em.close();
+        emf.close();
+    }
     
+    public void cons(int codi)
+    {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("POOPU");
+        EntityManager em = emf.createEntityManager();
+        try
+        {
+            this.objeAlum = em.find(Alumnos.class, codi);
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            em.close();
+            emf.close();            
+        }
+    }
 }
